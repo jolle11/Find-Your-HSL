92c3ec885b84f509ad212e0708d6d1ed673c8250
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2022-02-10 21:49:07 +0100

    Code refactoring and optimization

--- script.js
+++ script.js
@@ -12,24 +12,17 @@ const aElements = document.querySelectorAll('a');
 
 randomBtn.addEventListener('click', randomColor);
 copyBtn.addEventListener('click', copyColor);
-hInput.addEventListener('change', hUpdate);
-sInput.addEventListener('change', sUpdate);
-lInput.addEventListener('change', lUpdate);
+hInput.addEventListener('change', hInputUpdate);
+sInput.addEventListener('change', sInputUpdate);
+lInput.addEventListener('change', lInputUpdate);
 
 // Functions
-
-// Function to make sure every time we load or refresh the page,
-// all three values are reset to the predefined ones.
 
 window.onload = function () {
     hInput.value = 0;
     sInput.value = 0;
     lInput.value = 90;
 };
-
-// The randomColor function makes the random button work,
-// applying a random number from 0 to 360 for the HUE,
-// 0 to 100 for the saturation and 0 to 100 for the lightness.
 
 function randomColor() {
     let hValue = Math.floor(Math.random() * 361);
@@ -42,11 +35,8 @@ function randomColor() {
     let newLValue = lInput;
     newLValue.value = lValue;
     document.body.style.background = `hsl(${hValue}, ${sValue}%, ${lValue}%)`;
-    compColor();
+    textColorInversions();
 }
-
-// The copyColor function copies the color displayed in the correct format >> hsl(XXX, XXX%, XXX%)
-// Ready to paste in your editor.
 
 function copyColor() {
     hslColorCopy = `hsl(${hInput.value}, ${sInput.value}%, ${lInput.value}%)`;
@@ -57,54 +47,45 @@ function copyColor() {
     textArea.select();
     document.execCommand('copy');
     document.body.removeChild(textArea);
-    alert('Color code copied to the clipboard :D');
+    alert('Color code copied :D');
 }
 
-// The next 3 functions (hUpdate, sUpdate and lUpdate) work the same way.
-// The color is updated when you modify one of the values separately.
-// It also gives you an alert if the value is not correct for each field.
-
-function hUpdate() {
-    console.log(hInput.value);
+function hInputUpdate() {
     if (hInput.value >= 0 && hInput.value <= 360) {
         document.body.style.background = `hsl(${hInput.value}, ${sInput.value}%, ${lInput.value}%)`;
-        compColor();
+        textColorInversions();
     } else {
         alert('Not valid! Must be a number between 0 and 360.');
     }
 }
 
-function sUpdate() {
-    console.log(sInput.value);
+function sInputUpdate() {
     if (sInput.value >= 0 && sInput.value <= 100) {
         document.body.style.background = `hsl(${hInput.value}, ${sInput.value}%, ${lInput.value}%)`;
-        compColor();
+        textColorInversions();
     } else {
         alert('Not valid! Must be a number between 0 and 100.');
     }
 }
 
-function lUpdate() {
-    console.log(lInput.value);
+function lInputUpdate() {
     if (lInput.value >= 0 && lInput.value <= 100) {
         document.body.style.background = `hsl(${hInput.value}, ${sInput.value}%, ${lInput.value}%)`;
-        compColor();
+        textColorInversions();
     } else {
         alert('Not valid! Must be a number between 0 and 100.');
     }
 }
 
-// The compColor function modifies the color of the icons and letters on the screen
-// with the complementary color of the generated for the background.
+function textColorInversions() {
+    assignInvertedColor(pElements);
+    assignInvertedColor(aElements);
+}
 
-function compColor() {
+function assignInvertedColor(elements) {
     let bbg = `hsl(${hInput.value}, ${sInput.value}%, ${lInput.value}%)`;
-    pElements.forEach((pElement) => {
-        pElement.style.color = bbg;
-        pElement.style.filter = `invert(100%) hue-rotate(180deg)`;
-    });
-    aElements.forEach((aElement) => {
-        aElement.style.color = bbg;
-        aElement.style.filter = `invert(100%) hue-rotate(180deg)`;
+    elements.forEach((element) => {
+        element.style.color = bbg;
+        element.style.filter = `invert(100%) hue-rotate(180deg)`;
     });
 }
